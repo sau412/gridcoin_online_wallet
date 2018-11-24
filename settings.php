8d242ccd8525b1b4c7af84e3fc7b2886554f0e52
Tsarev Vladimir <sau412@gmail.com>
2018-11-24 17:41:56 +0300

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,35 @@
+<?php
+// Settings file
+
+$wallet_name="Gridcoin wallet (testing)";
+$wallet_domain="";
+$currency_short="GRC";
+$currency="Gridcoin";
+
+// DB variables
+$db_host="";
+$db_login="";
+$db_password="";
+$db_base="";
+
+// Salt for password
+$salt="";
+
+// Email service
+$email_api_url="https://api.smtp2go.com/v3/email/send";
+$email_api_key="";
+$email_sender="";
+$email_reply_to="";
+$email_notify="";
+
+// Gridcoin RPC variables
+$grc_rpc_host="";
+$grc_rpc_port="";
+$grc_rpc_login="";
+$grc_rpc_password="";
+$grc_rpc_wallet_passphrase="";
+
+// ReCAPTHCA
+$recaptcha_private_key="";
+$recaptcha_public_key="";
+?>
