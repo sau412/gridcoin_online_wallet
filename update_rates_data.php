1c882b875a38ea7dd68daa8872d39f45f8815ca2
Tsarev Vladimir <sau412@gmail.com>
2018-11-26 17:51:53 +0300

    Update exchange rates

---
+++ update_rates_data.php
@@ -0,0 +1,39 @@
+<?php
+require_once("settings.php");
+require_once("db.php");
+require_once("core.php");
+
+db_connect();
+
+// Setup cURL
+$ch=curl_init();
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+curl_setopt($ch,CURLOPT_FOLLOWLOCATION,TRUE);
+curl_setopt($ch,CURLOPT_POST,FALSE);
+
+// Get XMR price
+curl_setopt($ch,CURLOPT_URL,"https://api.coingecko.com/api/v3/coins/gridcoin-research");
+$result=curl_exec($ch);
+if($result=="") {
+        echo "No GRC price data\n";
+        log_write("No GRC price data");
+        die();
+}
+$parsed_data=json_decode($result);
+$btc_per_grc_price=(string)$parsed_data->market_data->current_price->btc;
+$usd_per_grc_price=(string)$parsed_data->market_data->current_price->usd;
+$rub_per_grc_price=(string)$parsed_data->market_data->current_price->rub;
+$ltc_per_grc_price=(string)$parsed_data->market_data->current_price->ltc;
+$xrp_per_grc_price=(string)$parsed_data->market_data->current_price->xrp;
+$eth_per_grc_price=(string)$parsed_data->market_data->current_price->eth;
+$xlm_per_grc_price=(string)$parsed_data->market_data->current_price->xlm;
+
+set_variable("btc_per_grc",$btc_per_grc_price);
+set_variable("usd_per_grc",$usd_per_grc_price);
+set_variable("rub_per_grc",$rub_per_grc_price);
+set_variable("ltc_per_grc",$ltc_per_grc_price);
+set_variable("xrp_per_grc",$xrp_per_grc_price);
+set_variable("eth_per_grc",$eth_per_grc_price);
+set_variable("xlm_per_grc",$xlm_per_grc_price);
+
+?>
