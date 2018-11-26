4f8cb2f13ac305768665266791fb55ecac99434d
Tsarev Vladimir <sau412@gmail.com>
2018-11-26 17:51:18 +0300

    Also saved received by address

--- update_gridcoin_data.php
+++ update_gridcoin_data.php
@@ -6,6 +6,22 @@ require_once("gridcoin.php");
 
 db_connect();
 
+// Get addresses received
+echo "Updating received amounts\n";
+$addresses_array=db_query_to_array("SELECT `uid`,`address` FROM `wallets` WHERE `address`<>'' AND `address` IS NOT NULL");
+
+foreach($addresses_array as $address_data) {
+        $uid=$address_data['uid'];
+        $address=$address_data['address'];
+        $address_received=grc_rpc_get_received_by_address($address);
+        $uid_escaped=db_escape($uid);
+        $address_received_escaped=db_escape($address_received);
+        echo "Address $address received $address_received\n";
+        db_query("UPDATE `wallets` SET `received`='$address_received_escaped' WHERE `uid`='$uid_escaped' AND `received`<'$address_received_escaped'");
+}
+
+// Generating new addresses
+echo "Generating new addresses\n";
 $addresses_array=db_query_to_array("SELECT `uid`,`user_uid` FROM `wallets` WHERE `address`='' OR `address` IS NULL");
 
 foreach($addresses_array as $address_data) {
@@ -13,9 +29,12 @@ foreach($addresses_array as $address_data) {
         $address=grc_rpc_get_new_address();
         $uid_escaped=db_escape($uid);
         $address_escaped=db_escape($address);
+        echo "New address $address\n";
         db_query("UPDATE `wallets` SET `address`='$address_escaped' WHERE `uid`='$uid_escaped' AND (`address`='' OR `address` IS NULL)");
 }
 
+// Syncronizing transactions
+echo "Synchronizing transactions\n";
 $transactions_array=grc_rpc_get_transactions();
 
 foreach($transactions_array as $transaction_data) {
@@ -49,6 +68,7 @@ ON DUPLICATE KEY UPDATE `status`=VALUES(`status`)");
 }
 
 // Send pending transactions
+echo "Sending transactions\n";
 $transactions_to_send=db_query_to_array("SELECT `uid`,`user_uid`,`amount`,`address` FROM `transactions` WHERE `status`='processing'");
 
 if(count($transactions_to_send)==0) {
@@ -75,11 +95,14 @@ foreach($transactions_to_send as $tx_data) {
                 $tx_id=grc_rpc_send($address,$amount);
 
                 if($tx_id==NULL || $tx_id==FALSE) {
+                        echo "Sending error to address $address amount $amount\n";
                         db_query("UPDATE `transactions` SET `tx_id`='',`status`='sending error' WHERE `uid`='$uid_escaped'");
                 } else {
+                        echo "Sent to address $address amount $amount\n";
                         db_query("UPDATE `transactions` SET `status`='sent',`tx_id`='$tx_id' WHERE `uid`='$uid_escaped'");
                 }
         } else {
+                echo "Address error to address $address amount $amount\n";
                 db_query("UPDATE `transactions` SET `tx_id`='',`status`='address error' WHERE `uid`='$uid_escaped'");
         }
         update_user_balance($user_uid);
