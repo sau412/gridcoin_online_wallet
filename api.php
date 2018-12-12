f31425a4730eeb1d7ea763529b29acba92434987
Tsarev Vladimir <sau412@gmail.com>
2018-12-12 10:36:11 +0300

    Added logging to each action

--- api.php
+++ api.php
@@ -30,22 +30,26 @@ switch($method) {
         // Get balance
         case 'get_balance':
                 $balance=get_user_balance($user_uid);
+                write_log("API: balance, result: '$balance'",$user_uid);
                 echo json_encode(array("balance"=>$balance));
                 break;
         // Get current_price in BTC
         case 'get_price_in_btc':
                 $btc_per_grc=get_variable("btc_per_grc");
+                write_log("API: get_price_in_btc, result: '$btc_per_grc'",$user_uid);
                 echo json_encode(array("btc_per_grc"=>$btc_per_grc));
                 break;
         // Get current_price in USD
         case 'get_price_in_usd':
                 $usd_per_grc=get_variable("usd_per_grc");
+                write_log("API: usd_per_grc, result: '$usd_per_grc'",$user_uid);
                 echo json_encode(array("usd_per_grc"=>$usd_per_grc));
                 break;
         // Get all prices
         case 'get_prices_all':
                 $btc_per_grc=get_variable("btc_per_grc");
                 $usd_per_grc=get_variable("usd_per_grc");
+                write_log("API: get_prices_all, result: btc_per_usd '$btc_per_grc', usd_per_grc '$usd_per_grc'",$user_uid);
                 echo json_encode(array(
                         "btc_per_usd"=>$btc_per_grc,
                         "usd_per_grc"=>$usd_per_grc
@@ -57,6 +61,7 @@ switch($method) {
         case 'get_all_receiving_addresses':
                 $user_uid_escaped=db_escape($user_uid);
                 $payout_addresses=db_query_to_array("SELECT `uid`,`address`,`received` FROM `wallets` WHERE `user_uid`='$user_uid_escaped'");
+                write_log("API: get_all_receiving_addresses",$user_uid);
                 echo json_encode($payout_addresses);
                 break;
         // Get specific receiving address
@@ -68,11 +73,13 @@ switch($method) {
                 $user_uid_escaped=db_escape($user_uid);
                 $payout_addresses=db_query_to_array("SELECT `uid`,`address`,`received` FROM `wallets` WHERE `user_uid`='$user_uid_escaped' AND `uid`='$address_uid_escaped'");
                 $payout_address_single=array_pop($payout_addresses);
+                write_log("API: get_receiving_address_by_uid '$address_uid'",$user_uid);
                 echo json_encode($payout_address_single);
                 break;
         // Query new receiving address
         case 'new_receiving_address':
                 $requiest_uid=user_create_new_address($user_uid);
+                write_log("API: new_receiving_address",$user_uid);
                 echo json_encode(array("uid"=>$requiest_uid));
                 break;
 
@@ -81,6 +88,7 @@ switch($method) {
         case 'get_all_transactions':
                 $user_uid_escaped=db_escape($user_uid);
                 $transactions_array=db_query_to_array("SELECT `uid`,`amount`,`address`,`status`,`tx_id`,`timestamp` FROM `transactions` WHERE `user_uid`='$user_uid_escaped'");
+                write_log("API: get_all_transactions",$user_uid);
                 echo json_encode($transactions_array);
                 break;
         // Get specific transaction
@@ -91,6 +99,7 @@ switch($method) {
                 $transaction_uid_escaped=db_escape($transaction_uid);
                 $transactions_array=db_query_to_array("SELECT `uid`,`amount`,`address`,`status`,`tx_id`,`timestamp` FROM `transactions` WHERE `user_uid`='$user_uid_escaped' AND `uid`='$transaction_uid_escaped'");
                 $transaction_single=array_pop($transactions_array);
+                write_log("API: get_transaction_by_uid '$transaction_uid'",$user_uid);
                 echo json_encode($transaction_single);
                 break;
         // Send specific amount to address
@@ -104,11 +113,13 @@ switch($method) {
 
                 $transaction_uid=user_send($user_uid,$amount,$address);
                 if($transaction_uid==FALSE) die("Sending error");
+                write_log("API: send '$amount' to '$address'",$user_uid);
                 echo json_encode(array("uid"=>$transaction_uid));
                 break;
 
         // Unknown method
         default:
+                write_log("API: Unknown method",$user_uid);
                 echo json_encode(array("error"=>"Unknown method"));
                 break;
 }
