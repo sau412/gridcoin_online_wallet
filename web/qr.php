bbe3e9e92d2f34a08aa99f80f35e0841794ef7ff
sau412 <sau412@gmail.com>
2019-12-27 12:00:39 +0300

    Fix QR encoder for hashes

--- web/qr.php
+++ web/qr.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-type: image/png");
 $str=stripslashes($_GET['str']);
-if(preg_match('/^[0-9A-Za-z]{1,50}$/',$str)) {
+if(preg_match('/^[0-9A-Za-z]{1,64}$/',$str)) {
 	passthru("qrencode '$str' -o -");
 }
 ?>
