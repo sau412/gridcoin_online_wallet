43df74e9cf63c00d1b3767ceab0507f6741293dc
sau412 <sau412@protonmail.com>
2020-06-20 23:02:01 +0300

    mysql_error polyfill added

--- lib/db.php
+++ lib/db.php
@@ -108,5 +108,9 @@ if(!function_exists("mysql_pconnect")) {
                 global $mysqli_res;
                 return mysqli_insert_id($mysqli_res);
         }
+        function mysql_error() {
+                global $mysqli_res;
+                return mysqli_error($mysqli_res);
+        }
 }
 ?>
