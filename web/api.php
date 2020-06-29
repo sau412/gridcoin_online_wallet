a73c8bc0a2e08420ee78be9042d7325d3c8415e0
sau412 <sau412@protonmail.com>
2020-06-29 18:08:53 +0300

    Email sending via broker

--- web/api.php
+++ web/api.php
@@ -3,6 +3,8 @@ require_once("../lib/settings.php");
 require_once("../lib/db.php");
 require_once("../lib/core.php");
 require_once("../lib/email.php");
+require_once("../lib/broker.php");
+require_once("../lib/logger.php");
 
 //$_POST=$_GET;
 
