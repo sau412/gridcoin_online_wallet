96fdd124d93538ee62e7d36cf3f9bc54faf295f0
sau412 <sau412@protonmail.com>
2020-11-22 16:39:19 +0300

    class="form-control"

--- lib/html.php
+++ lib/html.php
@@ -229,11 +229,11 @@ function html_address_book($user_uid,$token,$form=TRUE,$limit=10) {
 <input type=hidden name=token value='$token'>
 <div class='form-group'>
 <label for='alias_name'>%address_book_label%</label>
-<input type=text name=name id=alias_name>
+<input type=text class="form-control" name=name id=alias_name>
 </div>
 <div class='form-group'>
 <label for='alias_address'>%address_book_address%</label>
-<input type=text size=40 name=address id='alias_address'>
+<input type=text class="form-control" size=40 name=address id='alias_address'>
 </div>
 <input type=submit class='btn btn-primary' value='%address_book_submit%'></p>
 </form>
