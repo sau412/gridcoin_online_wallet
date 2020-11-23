201e88e84eecf45fd580cfc45273e9a501c57802
sau412 <sau412@protonmail.com>
2020-11-23 10:35:39 +0300

    Download file script

--- web/script.js
+++ web/script.js
@@ -9,5 +9,15 @@ function download_transactions_csv(user_uid, token) {
         $.get("./?ajax=1&block=transactions_csv")
                 .done(function(data) {
                         console.log(data);
+                        // https://stackoverflow.com/questions/17564103/using-javascript-to-download-file-as-a-csv-file
+                        let downloadLink = document.createElement("a");
+                        let blob = new Blob(["\ufeff", data]);
+                        let url = URL.createObjectURL(blob);
+                        downloadLink.href = url;
+                        downloadLink.download = "data.csv";
+
+                        document.body.appendChild(downloadLink);
+                        downloadLink.click();
+                        document.body.removeChild(downloadLink);
                 });
 }
