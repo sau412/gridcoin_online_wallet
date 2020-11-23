28dfa927c6a9c07cc80a41f36e901c9a2fa45d85
sau412 <sau412@protonmail.com>
2020-11-23 09:29:43 +0300

    Rowc-cols-md-2

--- lib/html.php
+++ lib/html.php
@@ -195,7 +195,7 @@ function html_wallet_form($user_uid,$token) {
 
         $result = "";
         $result .= <<<_END
-<div class="row">
+<div class="row row-cols-1 row-cols-md-2">
 <div class="col">
 
 _END;
